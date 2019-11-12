4458389c721be476cc32cb649a5cca2b99a88e44
Greg Kezos <greg.kezos@gmail.com>
2019-11-12 16:11:55 -0700

    Palindrome done. no stringify garbage.

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react'
 
 class Palindrome extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userInput: '',
+      palindrome: ''
+    }
+  }
+
+  updateUserInput(val) {
+    this.setState({ userInput: val})
+  }
+
+  checkPalindrome(input) {
+    let revInput = [...input].reverse().join('')
+    input === revInput
+      ? this.setState({ palindrome: 'true' })
+      : this.setState({ palindrome: 'false' })
+
+  }
+
   render() {
     return(
-      <p>Palindrome</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>Palindrome</h4>
+        <input className="inputLine" onChange={ ( e ) => { this.updateUserInput(e.target.value) }} />
+        <button className="confirmationButton" onClick={ () => this.checkPalindrome(this.state.userInput)}>Check</button>
+        <span className="resultsBox">Palindrome: { this.state.palindrome}</span>
+      </div>
     )
   }
 }
